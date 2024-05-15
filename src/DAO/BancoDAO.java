2b3d0182e4d43ea274c645fa6a0dd189974ab620
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-15 15:45:25 -0300

    login funcional ao banco de dados

--- src/DAO/BancoDAO.java
+++ src/DAO/BancoDAO.java
@@ -72,5 +72,16 @@ public class BancoDAO {
         statement.setString(1, moeda.getNome());
         statement.execute();
         conn.close();
-      }    
+      }
+      
+        public ResultSet consultarLogin(Investidor investidor) throws SQLException{
+            String sql = "select * from aluno where \"CPF\" = ? and \"Senha\" = ?";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            statement.setString(1, investidor.getCpf());
+            statement.setString(2, investidor.getSenha());
+            statement.execute();
+            ResultSet resultado = statement.getResultSet();
+            return resultado;
+        
+    }
 }
--- src/controller/ControllerLogin.java
+++ src/controller/ControllerLogin.java
@@ -4,9 +4,17 @@
  */
 package controller;
 
+import DAO.BancoDAO;
+import DAO.Conexao;
 import model.Administrador;
+import model.Investidor;
 import view.BemVindoAdministrador;
 import view.Login;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+import view.BemVindoUsuario;
 
 /**
  *
@@ -31,7 +39,29 @@ public class ControllerLogin {
            view.setVisible(false);
        }
        else{
-//           System.out.println("AAAAAA");
+        Investidor invest = new Investidor(null,cpfLogin,
+        senhaLogin);
+        Conexao conexao = new Conexao();
+        try{
+            Connection conn = conexao.getConnection();
+            BancoDAO dao = new BancoDAO(conn);
+            ResultSet res = dao.consultar(invest);
+            if(res.next()){
+                JOptionPane.showMessageDialog(view,"Login feito");
+                String nome = res.getString("Nome");
+                String cpf = res.getString("CPF");
+                String senha = res.getString("Senha");
+                
+                BemVindoUsuario b = new BemVindoUsuario(new Investidor(nome, cpf, senha));
+                b.setVisible(true);
+                view.setVisible(false);
+            }else{
+                JOptionPane.showMessageDialog(view,"Login não foi feito");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(view,"Erro de conexao");
+        }
+        
        }
     }    
 }

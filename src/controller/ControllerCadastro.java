4ea13e037e6d178741c2377eb1905a30da58ec08
LuaBortko <133054603+LuaBortko@users.noreply.github.com>
2024-05-20 13:05:07 -0300

    cpf e senha agora tem critérios

--- src/controller/ControllerCadastro.java
+++ src/controller/ControllerCadastro.java
@@ -27,19 +27,31 @@ public class ControllerCadastro {
         String nome = view.getTxtNomeInvest().getText();
         String cpf = view.getTxtCPFInvest().getText();
         String senha = view.getTxtSenhaInvest().getText();
-        
-        Investidor invest = new Investidor(nome,cpf,senha);
-        Conexao conexao = new Conexao();
-        
-        try{
-            Connection conn = conexao.getConnection();
-            BancoDAO dao = new BancoDAO(conn);
-            dao.inserir(invest);
-            JOptionPane.showMessageDialog(view,"Investidor cadastrado");
-      
-            
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(view,"Investidor nao cadastrado");
+        if (cpf.contains(".") || cpf.contains("-")) {
+        JOptionPane.showMessageDialog(view, "CPF não pode conter . ou -");
+        return;
+        }
+        if (senha.length() != 6) {
+            JOptionPane.showMessageDialog(view, "A senha deve conter 6 dígitos!");
+            return;
+        }
+        if(cpf.equals("1")){
+            JOptionPane.showMessageDialog(view,"Cpf inválido");
+        }
+        else{
+            Investidor invest = new Investidor(nome,cpf,senha);
+            Conexao conexao = new Conexao();
+
+            try{
+                Connection conn = conexao.getConnection();
+                BancoDAO dao = new BancoDAO(conn);
+                dao.inserir(invest);
+                JOptionPane.showMessageDialog(view,"Investidor cadastrado");
+
+
+            }catch(SQLException e){
+                JOptionPane.showMessageDialog(view,"Cpf inválido");
+            }
         }
     }
 }
